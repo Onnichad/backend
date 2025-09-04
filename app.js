e0083e3ba7e8b9253ca4cea706a8a6a08d008bc5
Onnichad <treil.christopher@outlook.com>
2025-09-04 17:00:58 -0400

    suite cours

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-
 const app = express();
-
 const mongoose = require("mongoose");
+
+const stuffRoutes = require("./routes/stuff");
+const userRoutes = require("./routes/user");
+
 mongoose
   .connect(
     "mongodb+srv://treilchristopher_db_user:chris123@cluster0.pimyc5o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
@@ -26,35 +28,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/stuff", (req, res, next) => {
-  console.log(req.body);
-  res.status(201).json({
-    message: "Objet creer",
-  });
-});
-
-app.get("/api/stuff", (req, res, next) => {
-  const stuff = [
-    {
-      _id: "oeihfzeoi",
-      title: "Mon premier objet",
-      description: "Les infos de mon premier objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 4900,
-      userId: "qsomihvqios",
-    },
-    {
-      _id: "oeihfzeomoihi",
-      title: "Mon deuxième objet",
-      description: "Les infos de mon deuxième objet",
-      imageUrl:
-        "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-      price: 2900,
-      userId: "qsomihvqios",
-    },
-  ];
-  res.status(200).json(stuff);
-});
+app.use("/api/stuff", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
