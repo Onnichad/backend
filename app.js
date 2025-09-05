beac60d8e33b24a563367a7c3930853bfa739062
Onnichad <treil.christopher@outlook.com>
2025-09-05 15:28:38 -0400

    fin du cours

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const stuffRoutes = require("./routes/stuff");
 const userRoutes = require("./routes/user");
+const path = require("path");
 
 mongoose
   .connect(
@@ -30,5 +31,6 @@ app.use((req, res, next) => {
 
 app.use("/api/stuff", stuffRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
